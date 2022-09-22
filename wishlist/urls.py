ed91cf9d0715b6a4d7cc284a78a6c65359cd3c20
I Made Urip Subagyo <uripsubagy@gmail.com>
2022-09-22 14:39:21 +0700

    Lab 3 tutorial

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,7 +3,9 @@ from wishlist.views import show_wishlist
 from wishlist.views import show_data_res
 from wishlist.views import show_data_json
 from wishlist.views import show_id_json
-
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 
 app_name = 'wishlist'
@@ -13,4 +15,7 @@ urlpatterns = [
     path('xml/', show_data_res, name='show_data_res'),
     path('json/', show_data_json, name='show_data_json'),
     path('json/<int:id>', show_id_json, name='show_id_json'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
